1fc6fd8fd9f3d930e89c201fc46fcba07b7ff60c
Jonathan Stray <jonathanstray@gmail.com>
2017-11-21 19:15:23 -0800

    Update to new API

--- ignorefirstrows.py
+++ ignorefirstrows.py
@@ -1,15 +1,4 @@
-class Importable:
-    @staticmethod
-    def __init__(self):
-        pass
-
-    @staticmethod
-    def event():
-        pass
-
-    @staticmethod
-    def render(wf_module, table):
-        number_of_rows = wf_module.get_param_integer('numberofrows')
-        newtab = table.ix[number_of_rows:]
-        wf_module.set_ready(notify=False)
-        return newtab
+def render(table, params):
+    number_of_rows = params['numberofrows']
+    newtab = table.ix[number_of_rows:]
+    return newtab
